bc406bea9ad3ba208a2478e41a3ee867cbb78a23
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 17:05:27 +0000

    Update local_storage.js

--- src/storage/local_storage.js
+++ src/storage/local_storage.js
@@ -1,1 +1,25 @@
+const localStorage = {
+  data: {},
+
+  setItem: (key, value) => {
+    localStorage.data[key] = value;
+    console.log(`Stored ${key}: ${value}`);
+  },
+
+  getItem: (key) => {
+    return localStorage.data[key];
+  },
+
+  removeItem: (key) => {
+    delete localStorage.data[key];
+    console.log(`Removed ${key}`);
+  },
+
+  clear: () => {
+    localStorage.data = {};
+    console.log('Local storage cleared');
+  }
+};
+
+module.exports = localStorage;
 
